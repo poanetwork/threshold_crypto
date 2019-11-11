cc40b5659f9cd41396d20199a17f757d0f77586b
Weiliang Li <to.be.impressive@gmail.com>
2019-11-11 13:29:16 +0900

    fix codec build error under feature std

--- src/codec_impl.rs
+++ src/codec_impl.rs
@@ -12,7 +12,9 @@ macro_rules! impl_codec_for {
         impl codec::Decode for $type {
             fn decode<I: codec::Input>(value: &mut I) -> std::result::Result<Self, codec::Error> {
                 let decoded: Vec<u8> = codec::Decode::decode(value)?;
-                bincode::deserialize(decoded.as_slice()).map_err(|_| codec::Error)
+                bincode::deserialize(decoded.as_slice()).map_err(|_| {
+                    codec::Error::from("parity-scale-codec decode error in threshold_crypto")
+                })
             }
         }
     };
